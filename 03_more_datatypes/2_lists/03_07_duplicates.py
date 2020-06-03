2069985c08424f6f766a9c7dc74261bd4b686ec7
Daniel <daniel.friedeberg@gmail.com>
2020-06-03 01:01:05 -0400

    remove duplicates from list

--- 03_more_datatypes/2_lists/03_07_duplicates.py
+++ 03_more_datatypes/2_lists/03_07_duplicates.py
@@ -5,3 +5,6 @@ Write a script that removes all duplicates from a list.
 '''
 
 list_ = [1, 2, 3, 4, 3, 4, 5]
+no_duplicates=list(dict.fromkeys(list_))
+print(no_duplicates)
+#I just looked this up...
