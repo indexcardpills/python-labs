9fcd136715235f3b3d29fdb56f0e0e4f548fa4a6
Daniel <daniel.friedeberg@gmail.com>
2020-06-02 23:31:07 -0400

    largest number in list

--- 03_more_datatypes/2_lists/03_06_product_largest.py
+++ 03_more_datatypes/2_lists/03_06_product_largest.py
@@ -9,3 +9,29 @@ that we haven't looked at yet. See if you can figure it out, otherwise
 come back to this task after you have learned about loops)
 
 '''
+first_number=input("enter a number: ")
+int(first_number)
+second_number=input("enter a number: ")
+int(second_number)
+third_number=input("enter a number: ")
+int(third_number)
+fourth_number=input("enter a number: ")
+int(fourth_number)
+fifth_number=input("enter a number: ")
+int(fifth_number)
+sixth_number=input("enter a number: ")
+int(sixth_number)
+seventh_number=input("enter a number: ")
+int(seventh_number)
+eighth_number=input("enter a number: ")
+int(eighth_number)
+ninth_number=input("enter a number: ")
+int(ninth_number)
+tenth_number=input("enter a number: ")
+int(tenth_number)
+list_of_numbers=[first_number, second_number, third_number, fourth_number, fifth_number, sixth_number,
+                 seventh_number, eighth_number, ninth_number, tenth_number]
+largest_number=max(list_of_numbers)
+print(largest_number)
+#could I have saved all the numbers into one variable and created a list out of that?
+
