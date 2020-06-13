361521688fbe86eacb1b6a03c7c734c44aefc044
Daniel <daniel.friedeberg@gmail.com>
2020-06-12 20:39:47 -0400

    trying to push some of conditionals and loops labs

--- 04_conditionals_loops/04_04_prime.py
+++ 04_conditionals_loops/04_04_prime.py
@@ -2,3 +2,13 @@
 Print out every prime number between 1 and 100.
 
 '''
+
+for number in range(1,100):
+   if number > 1:
+       for i in range(2,number):
+           if (number % i) == 0:
+               break
+       else:
+           print(number)
+
+    #had to look this up. I still don't really understand the for loops part of this...
--- 04_conditionals_loops/04_07_search.py
+++ 04_conditionals_loops/04_07_search.py
@@ -4,3 +4,15 @@ Receive a number between 0 and 1,000,000,000 from the user.
 Use while loop to find the number - when the number is found exit the loop and print the number to the console.
 
 '''
+while True:
+    number = int(input("Enter a number between 0 and 1,000,000,000: "))
+    x = 7
+    if number < x:
+        print("no, higher")
+    if number > x:
+        print("no, lower")
+    if number == x:
+        print("correct, the number is 7")
+        break
+#Is this what I was supposed to do? If not, I don't understand what is meant by "finding the number", if
+#I'm inputting it myself
