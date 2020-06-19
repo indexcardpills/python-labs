94032de15dba2d92dd41d9b330d0a5414944d184
Daniel <daniel.friedeberg@gmail.com>
2020-06-19 01:33:06 -0400

    first f string

--- 05_string_formatting/05_01_fstring.py
+++ 05_string_formatting/05_01_fstring.py
@@ -18,3 +18,31 @@ famous_quotes = [
     {"full_name": "Nathan Myhrvold", "quote": "Software is a gas; it expands to fill its container."},
     {"full_name": "Alan Bennett", "quote": "Standards are always out of date.  That’s what makes them standards."}
 ]
+first_item = famous_quotes[0]
+first_name=first_item["full_name"]
+first_quote=first_item["quote"]
+second_item=famous_quotes[1]
+second_name=second_item["full_name"]
+second_quote=second_item["quote"]
+third_item=famous_quotes[2]
+third_name=third_item["full_name"]
+third_quote=third_item["quote"]
+fourth_item=famous_quotes[3]
+fourth_name=fourth_item["full_name"]
+fourth_quote=fourth_item["quote"]
+fifth_item=famous_quotes[4]
+fifth_name=fifth_item["full_name"]
+fifth_quote=fifth_item["quote"]
+sixth_item=famous_quotes[5]
+sixth_name=sixth_item["full_name"]
+sixth_quote=sixth_item["quote"]
+seventh_item=famous_quotes[6]
+seventh_name=seventh_item["full_name"]
+seventh_quote=seventh_item["quote"]
+print(f'\"{first_quote}\" - {first_name}\n\"{second_quote}\" - {second_name}\n\"{third_quote}\" - {third_name}\n\
+\"{fourth_quote}\" - {fourth_name}\n\"{fifth_quote}\" - {fifth_name}\n\"{sixth_quote}\" - {sixth_name}\n\
+\"{seventh_quote}\" - {seventh_name}')
+# How would I make it print Lastname, Firstname?
+# Also, is there a faster/more efficient way of doing this? Or do I have to break it down into variables the
+# way I did here?
+
