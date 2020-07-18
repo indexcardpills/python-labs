9686314d8745c469356fb2f2cca557a6e38eddc8
Daniel <daniel.friedeberg@gmail.com>
2020-07-18 15:14:33 -0400

    first two i/o labs

--- 08_file_io/08_00_long_words.py
+++ 08_file_io/08_00_long_words.py
@@ -2,3 +2,17 @@
 Write a program that reads words.txt and prints only the words
 with more than 20 characters (not counting whitespace).
 '''
+words=[]
+with open('words.txt', 'r') as fin:
+    for word in fin.readlines():
+        word=word.rstrip()
+        words.append(word)
+    #print(words)
+
+    long_words = []
+    for w in words:
+        if len(w) > 20:
+            long_words.append(w)
+    print(long_words)
+
+
--- 08_file_io/08_01_words_analysis.py
+++ 08_file_io/08_01_words_analysis.py
@@ -7,3 +7,19 @@ Write a script that reads in the words from the words.txt file and finds and pri
 
 
 '''
+words=[]
+with open('words.txt', 'r') as fin:
+    for word in fin.readlines():
+        word=word.rstrip()
+        words.append(word)
+
+short_words=len(min(words, key=len))
+for s in words:
+    if len(s)==short_words:
+        print(s)
+long_words=(len(max(words, key=len)))
+for l in words:
+    if len(l)==long_words:
+        print(l)
+print(len(words))
+
