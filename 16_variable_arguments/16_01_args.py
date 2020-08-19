cb70724e9c5f238660d81563694967842b3bc2a7
Daniel <daniel.friedeberg@gmail.com>
2020-08-19 03:02:18 -0400

    args and kwargs

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -2,3 +2,10 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+def tennis_score(*args):
+    for x in args:
+        print(x)
+
+
+tennis_score('love', 15, 30, 40, 'deuce', 'advantage')
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,9 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+def tennis_score(**kwargs):
+    for x in kwargs.items():
+        print(x)
+tennis_score(zero='love', one=15, two=30, three=40)
+
